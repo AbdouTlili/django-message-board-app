b623d2b99119a1d760d621f1b8cb654ac22db95b
zacnnnt <zacnnnt@zacnnnt.zacnnnt.com>
2020-03-21 22:15:50 +0100

    adding model tests and the  templateview

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import Post
 
-# Create your tests here.
+class PostModelTest(TestCase):
+
+
+    def setUp(self):
+        Post.objects.create(text='testCase')
+
+    def test_text_content(self):
+        test_post = Post.objects.get(id=1)
+        expected_text = f'{test_post.text}'
+        self.assertEqual(expected_text,'testCase')
+        
